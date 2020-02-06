6854eafd44173471f9c3e0e96050649c73cb9c24
Ilya Petrov <dnamix1@gmail.com>
2020-02-06 13:30:06 +0300

    created graphql queries and mutations documents and manifested google consent sign in component with Login component

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -9,23 +9,29 @@ import { User } from './sections/User';
 import { Listing } from './sections/Listing';
 import { Host } from './sections/Host';
 import { Home } from './sections/Home';
+import { Login } from './sections/Login';
 import * as serviceWorker from './serviceWorker';
+import { Layout } from 'antd';
 import './styles/index.css';
 
 const client = new ApolloClient({
   uri: 'http://localhost:9000/api'
 });
+
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/host" component={Host} />
-        <Route exact path="/listing/:id" component={Listing} />
-        <Route exact path="/listings/:location?" component={Listings} />
-        <Route exact path="/user/:id" component={User} />
-        <Route component={NotFound} />
-      </Switch>
+      <Layout id="app">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/host" component={Host} />
+          <Route exact path="/listing/:id" component={Listing} />
+          <Route exact path="/listings/:location?" component={Listings} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/user/:id" component={User} />
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
     </Router>
   );
 };
