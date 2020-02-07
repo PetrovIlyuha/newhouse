09f4f559c5a02e3180aa84c4441d9efc2470039c
Ilya Petrov <dnamix1@gmail.com>
2020-02-07 13:26:01 +0300

    user notifications on successful logout and in the opposite case of encountering an error within that operation

--- client/src/sections/AppHeader/components/MenuItems/index.tsx
+++ client/src/sections/AppHeader/components/MenuItems/index.tsx
@@ -1,25 +1,55 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/react-hooks';
 import { Avatar, Button, Icon, Menu } from 'antd';
 import { LOG_OUT } from '../../../../lib/graphql/mutations/LogOut/index';
 import { LogOut as LogOutData } from '../../../../lib/graphql/mutations/LogOut/__generated__/LogOut';
 import { Viewer } from '../../../../lib/types';
+import {
+  displaySuccessNotification,
+  displayErrorMessage
+} from '../../../../lib/utils/index';
 const { Item, SubMenu } = Menu;
 
 interface Props {
   viewer: Viewer;
+  setViewer: (viewer: Viewer) => void;
 }
-export const MenuItems = ({ viewer }: Props) => {
+export const MenuItems = ({ viewer, setViewer }: Props) => {
+  const [logOut] = useMutation<LogOutData>(LOG_OUT, {
+    onCompleted: data => {
+      if (data && data.logOut) {
+        setViewer(data.logOut);
+        displaySuccessNotification(
+          "You've successfully logge Out! See you soon"
+        );
+      }
+    },
+    onError: data => {
+      displayErrorMessage(
+        'Sorry! Somehow we are unable to log you out! Try again after a while'
+      );
+    }
+  });
+
+  const handleLogOut = () => {
+    logOut();
+  };
+
   const subMenuLogin =
     viewer.id && viewer.avatar ? (
       <SubMenu title={<Avatar src={viewer.avatar} />}>
         <Item key="/user">
-          <Icon type="user" />
-          Profile
+          <Link to={`/user/${viewer.id}`}>
+            <Icon type="user" />
+            Profile
+          </Link>
         </Item>
         <Item key="/logout">
-          <Icon type="logout" />
-          Logout
+          <div onClick={handleLogOut}>
+            <Icon type="logout" />
+            Logout
+          </div>
         </Item>
       </SubMenu>
     ) : (
