49af3652954ab1c291044bebc16530cb294dfd5d
Ilya Petrov <dnamix1@gmail.com>
2020-02-12 15:11:25 +0300

    user page with skeleton pre-load & depending on the id of user displaying the additional section to become a rental owner

--- client/src/sections/User/components/UserProfile/index.tsx
+++ client/src/sections/User/components/UserProfile/index.tsx
@@ -1,10 +1,59 @@
 import React from 'react';
+import { Avatar, Card, Button, Divider, Typography } from 'antd';
 import { User as UserData } from '../../../../lib/graphql/queries/User/__generated__/User';
 
 interface Props {
   user: UserData['user'];
+  viewerIsUser: boolean;
 }
 
-export const UserProfile = ({ user }: Props) => {
-  return <h2>UserProfile </h2>;
+const { Paragraph, Text, Title } = Typography;
+
+export const UserProfile = ({ user, viewerIsUser }: Props) => {
+  const additionalDetailsSection = viewerIsUser ? (
+    <>
+      <Divider />
+      <div className="user-profile__details">
+        <Title level={4}>Additional Details</Title>
+        <Paragraph>
+          Interested in becoming FreshStart host? Register with your Stripe
+          account!
+        </Paragraph>
+        <Button type="primary" className="user-profile__details-cta">
+          Connect With Stripe
+        </Button>
+        <Paragraph type="secondary">
+          FreshStart uses{' '}
+          <a
+            href="https://stripe.com/en-US/connect"
+            target="_blank"
+            rel="noopener noreferre"
+          >
+            Stripe
+          </a>{' '}
+          to secure and transfer your payments and income
+        </Paragraph>
+      </div>
+    </>
+  ) : null;
+  return (
+    <div className="user-profile">
+      <Card className="user-profile__card">
+        <div className="user-profile__avatar">
+          <Avatar size={100} src={user.avatar} />
+        </div>
+        <Divider />
+        <div className="user-profile__details">
+          <Title level={4}>Details</Title>
+          <Paragraph>
+            Name: <Text strong>{user.name}</Text>
+          </Paragraph>
+          <Paragraph>
+            Contact: <Text strong>{user.contact}</Text>
+          </Paragraph>
+        </div>
+        {additionalDetailsSection}
+      </Card>
+    </div>
+  );
 };
--- client/src/sections/User/index.tsx
+++ client/src/sections/User/index.tsx
@@ -8,21 +8,40 @@ import {
   User as UserData,
   UserVariables
 } from '../../lib/graphql/queries/User/__generated__/User';
+import { Viewer } from '../../lib/types';
+import { PageSkeleton, ErrorBanner } from '../../lib/components';
 
+interface Props {
+  viewer: Viewer;
+}
 interface MatchParams {
   id: string;
 }
 
 const { Content } = Layout;
-export const User = ({ match }: RouteComponentProps<MatchParams>) => {
+export const User = ({
+  viewer,
+  match
+}: Props & RouteComponentProps<MatchParams>) => {
   const { data, loading, error } = useQuery<UserData, UserVariables>(USER, {
     variables: {
       id: match.params.id
     }
   });
 
+  if (loading) {
+    return (
+      <Content className="user">
+        <PageSkeleton />
+      </Content>
+    );
+  }
+
   const user = data ? data.user : null;
-  const userProfileElement = user ? <UserProfile user={user} /> : null;
+  const viewerIsUser = viewer.id === match.params.id;
+  const userProfileElement = user ? (
+    <UserProfile user={user} viewerIsUser={viewerIsUser} />
+  ) : null;
   return (
     <Content className="user">
       <Row gutter={12} type="flex" justify="space-between">
