cda207b0cbe498521690add250d248e3d418929a
Ilya Petrov <dnamix1@gmail.com>
2020-02-20 14:59:02 +0300

    showing invitation to create listing in the case of abscence of such after performing search on the main page and getting negative results from our database

--- client/src/sections/Listings/index.tsx
+++ client/src/sections/Listings/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { RouteComponentProps } from 'react-router-dom';
+import { RouteComponentProps, Link } from 'react-router-dom';
 import { useQuery } from '@apollo/react-hooks';
-import { Layout, List } from 'antd';
+import { Layout, Typography, List } from 'antd';
 import { ListingCard } from '../../lib/components';
 import { LISTINGS } from '../../lib/graphql/queries';
 import {
@@ -15,6 +15,7 @@ interface MatchParams {
 }
 
 const { Content } = Layout;
+const { Title, Paragraph, Text } = Typography;
 const PAGE_LIMIT = 8;
 
 export const Listings = ({ match }: RouteComponentProps<MatchParams>) => {
@@ -28,17 +29,40 @@ export const Listings = ({ match }: RouteComponentProps<MatchParams>) => {
   });
 
   const listings = data ? data.listings : null;
+  const listingsRegion = listings ? listings.region : null;
 
-  const listingsSectionElement = listings ? (
-    <List
-      grid={{ gutter: 8, sm: 2, lg: 4, xs: 1 }}
-      dataSource={listings.result}
-      renderItem={listing => (
-        <List.Item>
-          <ListingCard listing={listing} />
-        </List.Item>
-      )}
-    ></List>
+  const listingsSectionElement =
+    listings && listings.result.length ? (
+      <List
+        grid={{ gutter: 8, sm: 2, lg: 4, xs: 1 }}
+        dataSource={listings.result}
+        renderItem={listing => (
+          <List.Item>
+            <ListingCard listing={listing} />
+          </List.Item>
+        )}
+      ></List>
+    ) : (
+      <div>
+        <Paragraph>
+          "It appears to be no currently presented listings for this area"
+          <Text mark>"{listingsRegion}"</Text>
+        </Paragraph>
+        <Paragraph>
+          "You have the opportunity to post <Link to="/host">listings</Link> for
+          the area of"
+          <Text mark>"{listingsRegion}"</Text>
+        </Paragraph>
+      </div>
+    );
+
+  const listingsRegionElement = listingsRegion ? (
+    <Title>Results for "{listingsRegion}"</Title>
   ) : null;
-  return <Content className="listings">{listingsSectionElement}</Content>;
+  return (
+    <Content className="listings">
+      {listingsRegionElement}
+      {listingsSectionElement}
+    </Content>
+  );
 };
